26cefbaa84f536605c2ea2d1e9a2068c23fe9f30
Tim Gossett <tim@cloud.com>
2015-02-24 09:14:29 -0500

    add comments for godoc

--- metadata.go
+++ metadata.go
@@ -1,14 +1,14 @@
 package fireload
 
-// Metadata .
+// Metadata represents key-value pairs of metadata associated with a Namespace
 type Metadata map[string]interface{}
 
-// Set .
+// Set adds the given key-value pair
 func (m Metadata) Set(key string, value interface{}) {
 	m[key] = value
 }
 
-// Get .
+// Get retrieves the value stored under key.
 func (m Metadata) Get(key string) (interface{}, bool) {
 	val, ok := m[key]
 	return val, ok
